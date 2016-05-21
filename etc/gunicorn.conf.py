2b8aee43cf88e5f3370de37b621acc3eb8f67f85
noknod <noknod@mail.ru>
2016-05-21 22:00:01 +0500

    2.1.11

---
+++ etc/gunicorn.conf.py
@@ -0,0 +1,4 @@
+bind = ['0.0.0.0:8000']
+workers = 2
+pythonpath = '/home/box/web/ask'
+daemon = True
--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-echo $PATH
+#echo $PATH
 
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
@@ -7,3 +7,6 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf.hello.py /etc/gunicorn.d/conf.hello.py
 sudo gunicorn -c /etc/gunicorn.d/conf.hello.py hello:app
 #sudo /etc/init.d/gunicorn restart
+
+sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/conf.py
+sudo gunicorn -c /etc/gunicorn.d/conf.py ask.wsgi
